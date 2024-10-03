b18a1ec19309151b69b81f0edb952c3ceff82170
japprovato <jordana.approvato@gmail.com>
2024-10-02 22:23:39 -0700

    update starRating component to allow selecting rating

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
 import Star from "@/components/Star";
 
 const StarRating = () => {
+  const [rating, setRating] = useState<number>(0);
+  const [hoveredIndex, setHoveredIndex] = useState<number|null>(null);
+
   return (
     <section className="flex flex-col items-center">
       <div>
-        <Star starId={1} marked={false} />
-        <Star starId={2} marked={false} />
-        <Star starId={3} marked={false} />
-        <Star starId={4} marked={false} />
-        <Star starId={5} marked={false} />
-      </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <span
+            key={index}
+            tabIndex={0}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+            onClick={() => setRating(index + 1)}>
+            <Star
+              starId={index}
+              marked={
+                hoveredIndex != null
+                  ? index <= hoveredIndex
+                  : index < rating
+              }
+            />
+          </span>
+        ))}
+    </div>
       <input
         type="submit"
         className="mt-10 h-10 px-6 font-semibold rounded-md bg-black text-white"
